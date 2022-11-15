c76eded7b82985969b5bee38364d6391eddc27bc
Vlad <teftv81@gmail.com>
2022-11-15 12:52:58 +0300

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDDzaEVjXaQ3wKZhS6Yc1_2kMtAgQP5wMQ',
+  fbDbUrl: 'https://angular-blog-3ee7f-default-rtdb.firebaseio.com'
+};
